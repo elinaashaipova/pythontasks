29b44374838d38c1e4c78a2c723d675445851de8
user <elina.shaipova2020@gmail.com>
2023-04-20 15:31:48 +0300

    print 6

--- task1.py
+++ task1.py
@@ -1,4 +1,11 @@
 
+number = float(input("Введите число от 0 до 1000: "))
 
-x=6
-print (x)
+if number > 1000 or number < 0:
+    number = float(input("Ошибка! Неправильное число. Введите число от 0 до 1000: "))
+
+endnum = int(((number/2)*10)%10)
+if endnum == 0:
+    print ("True")
+else: print ("False")
+
